e90bae3c7566a32e8b9f471b239eb6c93b356ca5
balashiranjeevi <balashiranjeevi97@gmail.com>
2025-09-19 15:45:43 +0000

    First Commit.

---
+++ springapp/src/main/java/com/examly/springapp/DocumentManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package com.examly.springapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentManagementSystemApplication.class, args);
+		System.out.println("Application is working");
+	}
+
+}
